13de349afcf132defffebd7ea86931f2a3f4f1a9
User1 <user@example.com>
2020-04-16 14:43:13 -0500

    last update8

--- USMap/map2.js
+++ USMap/map2.js
@@ -1,4 +1,4 @@
-var USmap = L.map('LSUid').setView([40.122326, -99.813705], 4)
+var USmap = L.map('USid').setView([40.122326, -99.813705], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(USmap)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
