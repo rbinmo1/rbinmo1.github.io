8cbe2802e43a0c4862ae13b8d3256cefe91bab0e
User1 <user@example.com>
2020-04-16 14:40:04 -0500

    last update7

---
+++ USMap/map2.js
@@ -0,0 +1,14 @@
+var USmap = L.map('LSUid').setView([40.122326, -99.813705], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(USmap)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(USmap)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(USmap)
