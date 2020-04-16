93399b95731882a69ecec4da4fceb3ce62fb1518
User1 <user@example.com>
2020-04-16 08:23:51 -0500

    Last update3

--- LSUMap/map1.js
+++ LSUMap/map1.js
@@ -2,11 +2,11 @@ var LSUmap = L.map('LSUid').setView([30.405953, -91.183262], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(LSUmap)
 var marker = L.marker([30.412, -91.177]).addTo(LSUmap)
 var polygon = L.polygon([
-  [30.395, -91.204],
-  [30.419, -91.193],
-  [30.420, -91.171],
-  [30.410, -91.167],
-  [30.390, -91.790]
+  [30.395095, -91.205187],
+  [30.418413, -91.194114],
+  [30.4419301, -91.172142],
+  [30.403979, -91.175833],
+  [30.389838, -91.191025]
 ]).addTo(LSUmap)
 var polyline = L.polyline([
   [30.396681, -91.178379],
